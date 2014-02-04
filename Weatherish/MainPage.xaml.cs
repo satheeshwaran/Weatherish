e144ae6a3040ff78962bb49ccfbee400817d1078
satheeshwaran <satheesh4590@gmail.com>
2014-02-04 02:32:11 -0800

    Added functionality for Flikcr BG Images

--- Weatherish/MainPage.xaml.cs
+++ Weatherish/MainPage.xaml.cs
@@ -27,6 +27,11 @@ namespace Weatherish
         public Image currentWeatherIcon;
         public string dailyForecastData;
         public string weeklyForecastData;
+        public string currentCity;
+        public string currentPlace;
+        public string currentCountry;
+        private string flickrApiKey = "8244c11a9c3b02b45c127873ac225958";
+
         // Constructor
         public MainPage()
         {
@@ -112,7 +117,6 @@ namespace Weatherish
             {
                 string apiData = JsonConvert.DeserializeObject(weatherAPIResult).ToString();
                 RootObject apiDataJson = JsonConvert.DeserializeObject<RootObject>(apiData);
-                locationTextBlock.Text = apiDataJson.name;
                 int temp = (int)apiDataJson.main.temp;
                 currentTemperatureBlock.Text = temp.ToString() + "°C ";
                 currentTemperatureCondtion.Text = apiDataJson.weather[0].description;
@@ -155,10 +159,10 @@ namespace Weatherish
                         position.Coordinate.Longitude);
 
                 currentCoordinate = gpsCoorCenter;
+                getCurrentPlaceAndWOEID();
                 getCurrentWeatherData();
                 getDailyForecastData();
                 getWeeklyForecastData();
-                //getBGImageFromFlickrSource();
 
             }
             catch (Exception ex)
@@ -175,52 +179,89 @@ namespace Weatherish
             }
 
         }
+        private async void getCurrentPlaceAndWOEID()
+        {
+            HttpClient client = new HttpClient();
+
+            // About licenses:
+            // http://www.flickr.com/services/api/flickr.photos.licenses.getInfo.html
+            /*
+             *  <license id="4" name="Attribution License" url="http://creativecommons.org/licenses/by/2.0/" />
+                <license id="5" name="Attribution-ShareAlike License" url="http://creativecommons.org/licenses/by-sa/2.0/" />
+                <license id="6" name="Attribution-NoDerivs License" url="http://creativecommons.org/licenses/by-nd/2.0/" />
+                <license id="7" name="No known copyright restrictions" url="http://flickr.com/commons/usage/" />
+             */
+            string[] licenses = { "4", "5", "6", "7" };
+            string license = String.Join(",", licenses);
+            license = license.Replace(",", "%2C");
+            double latitude = 0;
+            double longitude = 0;
+
+            if (!double.IsNaN(latitude))
+                latitude = Math.Round(latitude, 5);
+
+            if (!double.IsNaN(longitude))
+                longitude = Math.Round(longitude, 5);
+
+            // Your API key ... REPLACE THIS WITH YOURS:
+            // http://www.flickr.com/services/api/keys/
+
+            // Search API
+            // http://www.flickr.com/services/api/flickr.photos.search.html
+
+            string url = "http://api.flickr.com/services/rest/" +
+                "?method=flickr.places.findByLatLon" +
+                "&api_key={1}"+
+                "&lat=" +currentCoordinate.Latitude.ToString()+
+                "&lon=" + currentCoordinate.Longitude.ToString()+
+                "&format=json" +
+                "&nojsoncallback=1";
+
+            var baseUrl = string.Format(url,
+                license,
+                flickrApiKey,
+                currentCoordinate.Latitude,
+                currentCoordinate.Longitude);
+
+            string flickrResult = await client.GetStringAsync(baseUrl);
+            Console.WriteLine(flickrResult);
+            //ResultTextBlock.Text = flickrResult;
+
+            Location apiData = JsonConvert.DeserializeObject<Location>(flickrResult);
+
+            if (apiData.stat == "ok")
+            {
+                foreach(Place placeObject in apiData.places.place)
+                {
+                    locationTextBlock.Text = placeObject.woe_name;
+                    currentPlace = placeObject.woe_name;
+                    string[] cityName = placeObject.name.Split(',');
+                    if (cityName.Length > 0)
+                        currentCity = cityName[1];
+                    if (cityName.Length > 3)
+                        currentCountry = cityName[4];
+                    break;
+                }
+                getBGImageFromFlickrSource();
+
+             }
+        }
         private async void getBGImageFromFlickrSource()
         {
 
                 HttpClient client = new HttpClient();
 
-                // About licenses:
-                // http://www.flickr.com/services/api/flickr.photos.licenses.getInfo.html
-                /*
-                 *  <license id="4" name="Attribution License" url="http://creativecommons.org/licenses/by/2.0/" />
-                    <license id="5" name="Attribution-ShareAlike License" url="http://creativecommons.org/licenses/by-sa/2.0/" />
-                    <license id="6" name="Attribution-NoDerivs License" url="http://creativecommons.org/licenses/by-nd/2.0/" />
-                    <license id="7" name="No known copyright restrictions" url="http://flickr.com/commons/usage/" />
-                 */
-                string[] licenses = { "4", "5", "6", "7" };
-                string license = String.Join(",", licenses);
-                license = license.Replace(",", "%2C");
-                double latitude = 0;
-                double longitude = 0;
-
-                if (!double.IsNaN(latitude))
-                    latitude = Math.Round(latitude, 5);
-
-                if (!double.IsNaN(longitude))
-                    longitude = Math.Round(longitude, 5);
-
-                // Your API key ... REPLACE THIS WITH YOURS:
-                // http://www.flickr.com/services/api/keys/
-                string flickrApiKey = "8244c11a9c3b02b45c127873ac225958";
-
-                // Search API
-                // http://www.flickr.com/services/api/flickr.photos.search.html
-
                 string url = "http://api.flickr.com/services/rest/" +
-                    "?method=flickr.photos.search" +
-                    "&api_key={1}" +
+                    "?method=flickr.groups.pools.getPhotos" +
+                    "&api_key={0}" +
+                    "&group_id=1463451%40N25"+
                     "&privacy_filter=1"+
-                    "&tags=weather"+
-                    "&safe_search=1"+
+                    "&tags="+currentPlace+
                     "&format=json" +
                     "&nojsoncallback=1";
 
                 var baseUrl = string.Format(url,
-                    license,
-                    flickrApiKey,
-                    currentCoordinate.Latitude,
-                    currentCoordinate.Longitude);
+                    flickrApiKey);
 
                 string flickrResult = await client.GetStringAsync(baseUrl);
                 Console.WriteLine(flickrResult);
@@ -230,31 +271,144 @@ namespace Weatherish
 
                 if (apiData.stat == "ok")
                 {
-                    foreach (Photo data in apiData.photos.photo)
+                    if (apiData.photos.photo.Count > 0)
                     {
-                        // To retrieve one photo, use this format:
-                        //http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}{size}.jpg
+                        foreach (Photo data in apiData.photos.photo)
+                        {
+                            // To retrieve one photo, use this format:
+                            //http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}{size}.jpg
 
-                        string photoUrl = "http://farm{0}.staticflickr.com/{1}/{2}_{3}.jpg";
-                        Console.WriteLine(photoUrl);
+                            string photoUrl = "http://farm{0}.staticflickr.com/{1}/{2}_{3}.jpg";
+                            Console.WriteLine(photoUrl);
 
-                        string baseFlickrUrl = string.Format(photoUrl,
-                            data.farm,
-                            data.server,
-                            data.id,
-                            data.secret);
+                            string baseFlickrUrl = string.Format(photoUrl,
+                                data.farm,
+                                data.server,
+                                data.id,
+                                data.secret);
 
-                        ImageBrush b = new ImageBrush();
-                        b.ImageSource = new BitmapImage(new Uri(baseFlickrUrl));
-                        b.Opacity = 0.70;
-                        b.Stretch = Stretch.Fill;
-                        weatherishPanorma.Background = b;
+                            setPanormaBG(baseFlickrUrl);
+                            break;
+                        }
                     }
+                    else
+                    {
+                        getBGImageFromFlickrSourceForNearestCity();
+                    }
+                }           
+        }
+
+        private async void getBGImageFromFlickrSourceForNearestCity()
+        {
+
+            HttpClient client = new HttpClient();
+
+            string url = "http://api.flickr.com/services/rest/" +
+                "?method=flickr.groups.pools.getPhotos" +
+                "&api_key={0}" +
+                "&group_id=1463451%40N25" +
+                "&privacy_filter=1" +
+                "&tags=" + currentCity +
+                "&format=json" +
+                "&nojsoncallback=1";
+
+            var baseUrl = string.Format(url,
+                flickrApiKey);
+
+            string flickrResult = await client.GetStringAsync(baseUrl);
+            Console.WriteLine(flickrResult);
+            //ResultTextBlock.Text = flickrResult;
+
+            FlickrData apiData = JsonConvert.DeserializeObject<FlickrData>(flickrResult);
+
+            if (apiData.stat == "ok")
+            {
+                if (apiData.photos.photo.Count > 0)
+                {
+                    Random r = new Random();
+                    int rInt = r.Next(0, apiData.photos.photo.Count);
+                    Photo data = apiData.photos.photo[rInt];
+
+                    string photoUrl = "http://farm{0}.staticflickr.com/{1}/{2}_{3}.jpg";
+                    Console.WriteLine(photoUrl);
+
+                    string baseFlickrUrl = string.Format(photoUrl,
+                        data.farm,
+                        data.server,
+                        data.id,
+                        data.secret);
+
+                    setPanormaBG(baseFlickrUrl);
+                  
                 }
+                else
+                {
+                    getBGImageFromFlickrSourceForCountryAtleast();
+                }
+            }
 
             //AroundMeMap.SetView(new GeoCoordinate(41.8988D, -87.6231D), 17D);
 
-           
+
+        }
+
+        private async void getBGImageFromFlickrSourceForCountryAtleast()
+        {
+
+            HttpClient client = new HttpClient();
+
+            string url = "http://api.flickr.com/services/rest/" +
+                "?method=flickr.groups.pools.getPhotos" +
+                "&api_key={0}" +
+                "&group_id=1463451%40N25" +
+                "&privacy_filter=1" +
+                "&tags=" + currentCountry +
+                "&format=json" +
+                "&nojsoncallback=1";
+
+            var baseUrl = string.Format(url,
+                flickrApiKey);
+
+            string flickrResult = await client.GetStringAsync(baseUrl);
+            Console.WriteLine(flickrResult);
+            //ResultTextBlock.Text = flickrResult;
+
+            FlickrData apiData = JsonConvert.DeserializeObject<FlickrData>(flickrResult);
+
+            if (apiData.stat == "ok")
+            {
+                if (apiData.photos.photo.Count > 0)
+                {
+                    Random r = new Random();
+                    int rInt = r.Next(0, apiData.photos.photo.Count);
+                    Photo data = apiData.photos.photo[rInt];
+
+                    string photoUrl = "http://farm{0}.staticflickr.com/{1}/{2}_{3}.jpg";
+                    Console.WriteLine(photoUrl);
+
+                    string baseFlickrUrl = string.Format(photoUrl,
+                        data.farm,
+                        data.server,
+                        data.id,
+                        data.secret);
+
+                    setPanormaBG(baseFlickrUrl);
+
+                }
+            }
+
+            //AroundMeMap.SetView(new GeoCoordinate(41.8988D, -87.6231D), 17D);
+
+
+        }
+
+        private void setPanormaBG(string imgURL)
+        {
+             ImageBrush b = new ImageBrush();
+                    b.ImageSource = new BitmapImage(new Uri(imgURL));
+                    b.Opacity = 0.70;
+                    b.Stretch = Stretch.Fill;
+                    weatherishPanorma.Background = b;
         }
 
         private void textBoxLoaded(object sender, RoutedEventArgs e)
@@ -370,4 +524,32 @@ namespace Weatherish
         public double cod { get; set; }
     }
 
+    public class Place
+    {
+        public string place_id { get; set; }
+        public string woeid { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public string place_url { get; set; }
+        public string place_type { get; set; }
+        public int place_type_id { get; set; }
+        public string timezone { get; set; }
+        public string name { get; set; }
+        public string woe_name { get; set; }
+    }
+
+    public class Places
+    {
+        public List<Place> place { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public int accuracy { get; set; }
+        public int total { get; set; }
+    }
+
+    public class Location
+    {
+        public Places places { get; set; }
+        public string stat { get; set; }
+    }
 }
